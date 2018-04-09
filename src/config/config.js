3f03b9976cca732e617d195cb249ee611aed9027
Catherine <lixiaoyin5@hotmail.com>
2018-04-09 20:57:43 +0800

    update accordance 3 options to 2 options update save unclean answers api

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 export default {
-    ipAddress: 'http://192.168.31.58:3001/api/',
-    getAllQuestionUrl: 'http://192.168.31.58:3001/api/question',
-    getRandamAnswerUrl: 'http://192.168.31.58:3001/api/fetchRandomAnswer',
-    getRandamFakeAnswerUrl: 'http://192.168.31.58:3001/api/fetchRandomFakeAnswer',
-    getSaveUncleanAnswerUrl: 'http://192.168.31.58:3001/api/saveUncleanAnswer',
+    ipAddress: 'http://localhost:3001/api/',
+    getAllQuestionUrl: 'http://localhost:3001/api/question',
+    getRandamAnswerUrl: 'http://localhost:3001/api/fetchRandomAnswer',
+    getRandamFakeAnswerUrl: 'http://localhost:3001/api/fetchRandomFakeAnswer',
+    getSaveUncleanAnswerUrl: 'http://localhost:3001/api/saveUncleanAnswer',
 }
